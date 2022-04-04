7c3e3ad35fb5bf086e3d7f31d09d48d38df33a10
QuentinPham <qej.pham@gmail.com>
2022-04-04 10:51:06 +0200

    Mise à Jour 04/04/2022
    Affichage Formulaire de connexion et d'inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,23 +20,36 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstnameUser')
-            ->add('lastnameUser')
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('email', EmailType::class,[
+                'label' => 'Email',
+                'required' => true,
+            ])
+            ->add('firstnameUser', TextType::class,[
+                'label' => 'Prénom',
+                'required' => true,
+            ])
+            ->add('lastnameUser', TextType::class,[
+                'label' => 'Nom',
+                'required' => true,
+            ])
+            ->add('plainPassword', RepeatedType::class, [
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
+                'required' => true,
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Votre mot de passe'],
+                'second_options' => ['label' => 'Confirmer votre mot de passe'],
+                'invalid_message' => 'Vos mots de passe doivent correspondre !',
+                'help' => 'Votre mot de passe doit comporter au moins 6 caractères.',
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Veuillez saisir un mot de passe',
+                        'message' => 'Veuillez saisir votre mot de passe !',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
+                        'minMessage' => 'Vos mot de passe doit comporter au moins 6 caractères !',
                         'max' => 4096,
+                        'maxMessage' => 'Vos mot de passe doit être limité à 4096 caractères !',
                     ]),
                 ],
             ])
@@ -44,6 +60,7 @@ class RegistrationFormType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'attr' => ['id' => 'registerForm']
         ]);
     }
 }
